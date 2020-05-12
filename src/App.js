542d114554d209b82450ab4b73df9cfac15fdd3d
Dana G <32311532+dastryyy@users.noreply.github.com>
2020-05-12 19:45:47 -0400

    Added homepage template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
+import Homepage from './components/Homepage';
 
 
 const App = () => {
   return (
     <div>
-      Homepage
+      <Homepage/>
     </div>
   );
 }
