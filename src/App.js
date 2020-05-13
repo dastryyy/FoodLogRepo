e76f1d94ba31378781faa537309f00b4cd941451
Dana G <32311532+dastryyy@users.noreply.github.com>
2020-05-12 20:04:19 -0400

    Added registration form template (to be modified later

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Homepage from './components/Homepage';
+import Register from './components/Register';
 
 
 const App = () => {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path = "/">
+          <Homepage/>
+        </Route>
+        <Route path = "/register">
+          <Register/>
+        </Route>
+      </Switch>
     </div>
   );
 }
