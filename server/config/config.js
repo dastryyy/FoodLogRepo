e2e65d8097936a9ef423ec366cccd8ea1e488d47
Dana Graham <32311532+dastryyy@users.noreply.github.com>
2021-05-17 23:50:07 -0400

    Delete config.js

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-//This file holds any configuration variables we may need
-//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
-
-module.exports = {
-    db: {
-        uri: 'mongodb+srv://dgraham:tada123@cen3031-rmjxt.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-};
